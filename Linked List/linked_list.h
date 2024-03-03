56e5528e9b429f04c46d906fbab66a0771f342d2
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2024-03-03 15:40:34 +0200

    Add files via upload

---
+++ Linked List/linked_list.h
@@ -0,0 +1,27 @@
+/*linked_list: Dhlwseis apla syndedemenhs listas*/
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int elem;   /*  typos dedomenwn listas  */
+
+struct node{			/*  typos komvou listas  */
+	elem data;			/*  dedomena  */
+	struct node *next;  /*  epomenos  */
+};
+
+typedef struct node LIST_NODE;
+typedef struct node *LIST_PTR;
+
+void LL_init(LIST_PTR *head);
+int LL_empty(LIST_PTR head);
+elem LL_data(LIST_PTR p); 
+int LL_insert_start(LIST_PTR *head, elem x);
+int LL_insert_after(LIST_PTR p, elem x);
+int LL_delete_start(LIST_PTR *head, elem *x);
+int LL_delete_after(LIST_PTR prev, elem *x);
+void LL_print(LIST_PTR head);
+void LL_destroy(LIST_PTR *head);
+
+
+
