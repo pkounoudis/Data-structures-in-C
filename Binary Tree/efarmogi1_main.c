a81dcd118d6ca39d883e665cd5b2db52e544d7cc
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2024-03-04 18:09:55 +0200

    Add files via upload

---
+++ Binary Tree/efarmogi1_main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tree1.h"
+
+
+main()
+{
+	int elem;
+	TREE_PTR root;
+	
+	TR_init(&root);
+	
+	//Eisagwgh tou 10
+	TR_insert_root(&root, 10);
+	
+	//Eisagwgh epipleon stoixeiwn
+	TR_insert_left(root, 6);
+	TR_insert_right(root, 14);
+	
+	TR_insert_left(root->left, 5);
+	TR_insert_right(root->left, 7);
+	TR_insert_right(root->right, 19);
+	
+	TR_insert_left(root->left->left, 2);
+	TR_insert_right(root->left->right, 8);
+
+	TR_insert_right(root->left->left->left, 3);
+
+	
+	/*Ektypwsh toy dentroy*/
+	printf("\n PREORDER: ");
+	TR_preorder(root);	
+	
+	printf("\n INORDER: ");
+	TR_inorder(root);	
+	
+	printf("\n POSTORDER: ");
+	TR_postorder(root);		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Binary Tree/tree1.h
@@ -0,0 +1,38 @@
+/*tree.h: Dhlwseis dyadikoy dentrou*/
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int elem;   /*  typos dedomenwn dentroy  */
+
+struct node{			/*  typos komvou dentroy  */
+	elem data;			/*  dedomena  */
+	struct node *left;  /*  aristero paidi  */
+	struct node *right; /*  deksi paidi  */
+};
+
+typedef struct node TREE_NODE; /*Synwnymo toy komvoy dentrou*/
+typedef struct node *TREE_PTR;	/*Synwnymo toy deikth komvoy dentroy*/
+
+void TR_init(TREE_PTR *root);
+int TR_empty(TREE_PTR root);
+elem TR_data(TREE_PTR p);
+int TR_insert_root(TREE_PTR *root, elem x);
+int TR_insert_left(TREE_PTR node, elem x);
+int TR_insert_right(TREE_PTR node, elem x);
+int TR_delete_root(TREE_PTR *root, elem *x);
+int TR_delete_left(TREE_PTR parent, elem *x);
+int TR_delete_right(TREE_PTR parent, elem *x);
+void TR_preorder(TREE_PTR v);
+void TR_inorder(TREE_PTR v);
+void TR_postorder(TREE_PTR v);
+void TR_print_node(TREE_PTR v);
+
+int TR_search_BST(TREE_PTR root, elem x);
+int TR_insert_BST(TREE_PTR *root, elem x);
+
+
+
+
+
+
